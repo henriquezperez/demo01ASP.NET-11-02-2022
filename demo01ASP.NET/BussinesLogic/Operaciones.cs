b1b67a4dae8ded582b9276a97570481d8bed98c2
Miguel <hp0826022019@unab.edu.sv>
2022-02-19 00:40:38 -0600

    Update

---
+++ demo01ASP.NET/BussinesLogic/Operaciones.cs
@@ -0,0 +1,13 @@
+namespace demo01ASP.NET.Services;
+
+namespace demo01ASP.NET.Services{
+
+    public interface BussinesLogic{
+        
+        Double Suma(Acciones acciones);
+        Double Resta(Acciones acciones);
+        Double Multiplicacion(Acciones acciones);
+        Double Division(Acciones acciones);
+
+    }
+}
--- demo01ASP.NET/Services/IOperaciones.cs
+++ demo01ASP.NET/Services/IOperaciones.cs
@@ -1,7 +1,9 @@
 using demo01ASP.NET.Models;
 
 namespace demo01ASP.NET.Services{
-    public interface Operaciones{
+
+    public interface IOperaciones{
+        
         Double Suma(Acciones acciones);
         Double Resta(Acciones acciones);
         Double Multiplicacion(Acciones acciones);
